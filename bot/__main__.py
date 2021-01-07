999a900a38b2991cfa112c20780f1133f3fde9cf
rpo <pozzir00@gmail.com>
2021-01-07 14:14:30 +0100

    flow is ok. see todo and exceptions

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,8 @@
 from bot.Updater import Updater
-import os, sys, platform, subprocess
+import os
+import sys
+import platform
+import subprocess
 import tensorflow as tf
 import random
 import numpy as np
@@ -14,9 +17,42 @@ import bot.utils.filter_input as filter_input
 import bot.utils.PickleDBExtended
 from bot.utils import style_transfer
 
-unknown_threshold = 0
-blur_threshold = 0.3
-dark_threshold = 0.3
+BTN_STOP = '/stop'
+BTN_START = '/start'
+
+KEY_SIMILARITY = 'KEY_SIMILARITY_{}'
+KEY_STYLE_BASE_IMG = 'KEY_STYLE_BASE_{}'
+# only needed when quality check failed if users choose to continue
+KEY_SIMILAR_IMAGE_IMG = 'KEY_SIMILAR_IMAGE_{}'
+KEY_QUALITY_CONTINUE_PREVSTATE = 'KEY_QUALITY_CONTINUE_PREVSTATE_{}'
+
+STATE_TOSTART = 'STATE_TO_START'
+STATE_WAIT_ACTION = 'STATE_WAIT_ACTION'
+STATE_WAIT_SIMILAR_IMAGE = 'STATE_WAIT_SIMILAR'
+STATE_WAIT_STYLE_BASE = 'STATE_WAIT_STYLE_BASE'
+STATE_WAIT_STYLE_STYLE = 'STATE_WAIT_STYLE_STYLE'
+STATE_CHOOSE_SIMILARITY = 'STATE_CHOOSE_SIMILARITY'
+STATE_QUALITY_ASK_CONTINUE = 'STATE_QUALITY_ASK_CONTINUE'
+
+MSG_START = 'Hi, send /start to begin!'
+MSG_PROMPT_ACTION = 'What do you want to do?'
+MSG_SEND_FOR_SIMILAR = 'Send an image of a clothe'
+MSG_SEND_FOR_STYLE_BASE = 'Send an image of a clothe to which you want to apply a new style'
+MSG_SEND_FOR_STYLE_STYLE = 'Send a style texture'
+MSG_CHOOSE_SIMILARITY = 'Choose which kind of similarity to look for'
+MSG_QUALITY_CHECK_FAILED = "The image sent didn't pass the quality check due to {}. Continue anyway?"
+
+BTN_SEARCH_SIMILAR = 'Retrieve similar products'
+BTN_STYLE_TRANSFER = 'Apply style to a product'
+BTN_SIMIL_COLOR = 'by Color'
+BTN_SIMIL_SHAPE = 'by Shape'
+BTN_SIMIL_NEURAL = 'by Neural Net'
+BTN_YES = 'Yes'
+BTN_NO = 'No'
+
+UNKNOWN_THRESHOLD = 0
+BLUR_THRESHOLD = 0.3
+DARK_THRESHOLD = 0.3
 
 base_dir = '.'
 
@@ -30,78 +66,130 @@ model_path = os.path.join(data_dir, 'model.h5')
 
 db_path = os.path.join(data_dir, 'state.db')
 
+
 def fileparts(fn):
     (dirName, fileName) = os.path.split(fn)
     (fileBaseName, fileExtension) = os.path.splitext(fileName)
     return dirName, fileBaseName, fileExtension
 
+
 def textHandler(bot, message, chat_id, text):
     #print(f'{chat_id}: {message} text: {text}')
 
-    state = db.get(chat_id) or 'tostart'
+    state = db.get(chat_id) or STATE_TOSTART
 
     print('state', state)
 
-    if text == '/stop':
-        db.set(chat_id, 'tostart')
-        bot.sendMessage(chat_id, 'Hi, send /start to begin!', keyboard=[['/start']])
+    if text == BTN_STOP:
+        db.set(chat_id, STATE_TOSTART)
+        bot.sendMessage(chat_id, MSG_START, keyboard=[[BTN_START]])
 
-    elif state == 'tostart':
-        if text == '/start':
-            db.set(chat_id, 'wait4command')
-            bot.sendMessage(chat_id, 'What do you want to do?',
-                keyboard=[['send an image','nothing']])
+    elif state == STATE_TOSTART:
+        if text == BTN_START:
+            bot.sendMessage(chat_id, MSG_PROMPT_ACTION,
+                            keyboard=[
+                                [BTN_SEARCH_SIMILAR, BTN_STYLE_TRANSFER],
+                                [BTN_STOP]
+                            ])
+            db.set(chat_id, STATE_WAIT_ACTION)
         else:
-            bot.sendMessage(chat_id, 'Hi, send /start to begin!', keyboard=[['/start']])
+            bot.sendMessage(chat_id, MSG_START, keyboard=[[BTN_START]])
 
-    elif state == 'wait4command':
-        if text == 'send an image':
-            db.set(chat_id, 'wait4image')
-            bot.sendMessage(chat_id, 'Send an image then!')
+    elif state == STATE_WAIT_ACTION:
+        if text == BTN_SEARCH_SIMILAR:
+            bot.sendMessage(chat_id, MSG_CHOOSE_SIMILARITY,
+                            keyboard=[
+                                [BTN_SIMIL_COLOR, BTN_SIMIL_SHAPE, BTN_SIMIL_NEURAL],
+                                [BTN_STOP]
+                            ])
+            db.set(chat_id, STATE_CHOOSE_SIMILARITY)
+
+        elif text == BTN_STYLE_TRANSFER:
+            bot.sendMessage(chat_id, MSG_SEND_FOR_STYLE_BASE)
+            db.set(chat_id, STATE_WAIT_STYLE_BASE)
+
         else:
-            db.set(chat_id, 'tostart')
-            bot.sendMessage(chat_id, 'Hi, send /start to begin!', keyboard=[['/start']])
+            db.set(chat_id, STATE_TOSTART)
+            bot.sendMessage(chat_id, MSG_START, keyboard=[[BTN_START]])
 
-    elif state == 'wait4image':
-        bot.sendMessage(chat_id, "I'm waiting for your image...")       
+    elif state == STATE_CHOOSE_SIMILARITY:
+        if text not in [BTN_SIMIL_COLOR, BTN_SIMIL_SHAPE, BTN_SIMIL_NEURAL]:
+            # exception todo
+            pass
+
+        # note similarity for chat_id
+        db.set(KEY_SIMILARITY.format(chat_id), text)
+
+        bot.sendMessage(chat_id, MSG_SEND_FOR_SIMILAR)
+        db.set(chat_id, STATE_WAIT_SIMILAR_IMAGE)
+
+    elif state == STATE_QUALITY_ASK_CONTINUE:
+        if text == BTN_YES:
+            prevstate = db.get(KEY_QUALITY_CONTINUE_PREVSTATE.format(chat_id))
+
+            if prevstate == STATE_WAIT_STYLE_BASE:
+                bot.sendMessage(chat_id, MSG_SEND_FOR_STYLE_STYLE)
+                db.set(chat_id, STATE_WAIT_STYLE_STYLE)
+
+            elif prevstate == STATE_WAIT_SIMILAR_IMAGE:
+                img_path = db.get(KEY_SIMILAR_IMAGE_IMG.format(chat_id)) or None
+
+                if not img_path:
+                    #exception
+                    pass
+
+                similarity = db.get(KEY_SIMILARITY.format(chat_id)) or None
+                print('similarity', similarity)
+
+                if similarity not in [BTN_SIMIL_COLOR, BTN_SIMIL_SHAPE, BTN_SIMIL_NEURAL]:
+                    # exception todo
+                    pass
+                
+                do_similarity(bot, chat_id, similarity, img_path)
+
+                db.set(chat_id, STATE_TOSTART)
+
+            else:
+                # todo
+                pass
+    # else: far capire all utente cosa si aspetta
 
 
-
-def imageHandler(bot, message, chat_id, img_path):
-    state = db.get(chat_id) or 'tostart'
-    if state != 'wait4image':
-        bot.sendMessage(chat_id, 'Hi, send /start to begin!', keyboard=[['/start']])
-        return
-
-    state = db.set(chat_id, 'tostart')
-
-    print(img_path)
-
-    # quality check
-    quality_check = True
-    # blur
+def quality_control_blur_dark(img_path, blur_threshold, dark_threshold):
     is_blurred, sharpness = filter_input.is_blurred(img_path, blur_threshold)
     if is_blurred:
-        bot.sendMessage(chat_id, f"The image is blurred! Sharpness: {sharpness}")
-        print("image is blurred!")
-        quality_check = False
+        print(f"image is blurred! Sharpness: {sharpness}")
 
     is_dark, brightness = filter_input.is_dark(img_path, dark_threshold)
     if is_dark:
-        bot.sendMessage(chat_id, f"The image is dark! Brightness: {brightness}")
-        print("image is dark!")
-        quality_check = False
+        print(f"image is dark! brigthness: {brightness}")
 
-    if not quality_check:
-        bot.sendMessage(chat_id, "I'm sorry your image didn't pass the quality check!")
-        print("quality check failed!")
-        return        
+    return is_blurred, is_dark
+
+def do_style_transfer(bot, chat_id, img_style):
+    # fare cose
+    # ritornare immagini
+    img_base = db.get(KEY_STYLE_BASE_IMG.format(chat_id)) or None
+
+    if not img_base:
+        # exception
+        pass
+
+    bot.sendMessage(chat_id, 'dai implementami!')
+
+def do_similarity(bot, chat_id, similarity, img_path):
+    # faccio cose
+    # ritorno immagini
+
+    bot.sendMessage(chat_id, f"""
+chosen similarity: {similarity}
+""")
 
     X = loadimg(img_path)
     pred = model.predict(X)
 
     detected_class, confidence_lvl = softmax2class(
-        pred[0], classes, threshold=unknown_threshold)
+        pred[0], classes, threshold=UNKNOWN_THRESHOLD)
 
     detected_class = detected_class.upper()
 
@@ -119,7 +207,94 @@ I bet this is a **{detected_class}** with a confidence of {confidence_lvl}!
 
     bot.sendMediaGroup(chat_id, names, "Here some similar images!")
 
-    bot.sendMessage(chat_id, 'Hi, send /start to begin!', keyboard=[['/start']])
+    bot.sendMessage(chat_id, MSG_START, keyboard=[[BTN_START]])
+    
+
+
+def imageHandler(bot, message, chat_id, img_path):
+    state = db.get(chat_id) or STATE_TOSTART
+
+    if state == STATE_WAIT_STYLE_BASE:
+        # note img path
+        db.set(KEY_STYLE_BASE_IMG.format(chat_id), img_path)
+
+        # controllo input
+        is_blur, is_dark = quality_control_blur_dark(
+            img_path, BLUR_THRESHOLD, DARK_THRESHOLD)
+
+        # todo aggiungere controllo margini
+        if is_blur or is_dark:
+            if is_blur and is_dark:
+                cause = 'blurriness and darkness'
+            elif is_blur:
+                cause = 'blurriness'
+            elif is_dark:
+                cause = 'darkness'
+
+            bot.sendMessage(chat_id, MSG_QUALITY_CHECK_FAILED.format(cause),
+                            keyboard=[
+                [BTN_YES, BTN_NO],
+                [BTN_STOP]
+            ])
+
+            # annoto stato precedente
+            db.set(KEY_QUALITY_CONTINUE_PREVSTATE.format(chat_id), STATE_WAIT_STYLE_BASE)
+            
+            db.set(chat_id, STATE_QUALITY_ASK_CONTINUE)
+
+            # todo: se non continua controllare che immagine annotata non dia problemi
+        else:
+
+            bot.sendMessage(chat_id, MSG_SEND_FOR_STYLE_STYLE)
+            db.set(chat_id, STATE_WAIT_STYLE_STYLE)
+
+    elif state == STATE_WAIT_STYLE_STYLE:
+        do_style_transfer(bot, chat_id, img_path)
+
+        db.set(chat_id, STATE_TOSTART)
+
+    elif state == STATE_WAIT_SIMILAR_IMAGE:
+        # prendo la similarità scelta prima
+        similarity = db.get(KEY_SIMILARITY.format(chat_id)) or None
+        print('similarity', similarity)
+
+        if similarity not in [BTN_SIMIL_COLOR, BTN_SIMIL_SHAPE, BTN_SIMIL_NEURAL]:
+            # exception todo
+            pass
+
+        # controllo input
+        is_blur, is_dark = quality_control_blur_dark(
+            img_path, BLUR_THRESHOLD, DARK_THRESHOLD)
+
+        # todo aggiungere controllo margini
+        if is_blur or is_dark:
+            if is_blur and is_dark:
+                cause = 'blurriness and darkness'
+            elif is_blur:
+                cause = 'blurriness'
+            elif is_dark:
+                cause = 'darkness'
+
+            bot.sendMessage(chat_id, MSG_QUALITY_CHECK_FAILED.format(cause),
+                            keyboard=[
+                [BTN_YES, BTN_NO],
+                [BTN_STOP]
+            ])
+
+            # annoto stato precedente
+            db.set(KEY_QUALITY_CONTINUE_PREVSTATE.format(chat_id), STATE_WAIT_SIMILAR_IMAGE)
+            # annoto immagine
+            db.set(KEY_SIMILAR_IMAGE_IMG.format(chat_id), img_path)
+            
+            db.set(chat_id, STATE_QUALITY_ASK_CONTINUE)
+
+            # todo: se non continua controllare che immagine annotata non dia
+            # problemi
+        else:
+            do_similarity(bot, chat_id, similarity, img_path)
+
+            db.set(chat_id, STATE_TOSTART)
+
 
 def loadimg(img_path):
 
@@ -132,30 +307,32 @@ def loadimg(img_path):
 
     return np.array([imarr])
 
+
 def softmax2class(softmax, classes, threshold=0.5, unknown='unknown'):
-  max = softmax.max()
-  if max >= threshold:
-    argmax = softmax.argmax()
-    return classes[argmax], max
-  else:
-    return unknown, max
+    max = softmax.max()
+    if max >= threshold:
+        argmax = softmax.argmax()
+        return classes[argmax], max
+    else:
+        return unknown, max
+
 
 if __name__ == "__main__":
 
     model = tf.keras.models.load_model(model_path)
     classes = ['bag',
-                'elegant_jacket',
-                'high_heels_shoe',
-                'jacket',
-                'shoe',
-                'shorts',
-                'sweatshirt',
-                't_shirt',
-                'trousers',
-                'unknown'
-              ]
+               'elegant_jacket',
+               'high_heels_shoe',
+               'jacket',
+               'shoe',
+               'shorts',
+               'sweatshirt',
+               't_shirt',
+               'trousers',
+               'unknown'
+               ]
 
-    cfe = ColorFeaturesExtractor((24,26,3), 0.6)
+    cfe = ColorFeaturesExtractor((24, 26, 3), 0.6)
 
     retriever = Retriever(index_dir)
 
